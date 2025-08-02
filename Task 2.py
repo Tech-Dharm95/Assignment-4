f703506cf52bb48c1cf7a23ea3330fd35d5f057c
DHARMENDRA PRASAD <161959278+Tech-Dharm95@users.noreply.github.com>
2025-08-02 22:21:31 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,26 @@
+def write_to_file(filename):
+    user_input = input("Enter text to write to the file: ")
+    with open(filename, 'w') as file:
+        file.write(user_input + '\n')
+    print("Data successfully written to output.txt.")
+
+def append_to_file(filename):
+    additional_input = input("Enter additional text to append: ")
+    with open(filename, 'a') as file:
+        file.write(additional_input + '\n')
+    print("Data Successfully apended.")
+# Final content of output.txt:
+def read_file(filename):
+    print("\nFinal content of the file:")
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                print(line.strip())
+    except FileNotFoundError:
+        print(f"Error: The file '{filename}' was not found.")
+
+# Main program flow
+filename = "output.txt"
+write_to_file(filename)
+append_to_file(filename)
+read_file(filename)
